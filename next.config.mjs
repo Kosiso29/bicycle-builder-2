a158be6a00a711f646dbc2584e60a1e7e1f06c5c
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-03-17 23:18:25 +0100

    17/3/24: add more image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.cervelo.com', 'assets.specialized.com', 'enve.com', 'giant-bicycles.com', 'cervelo.com', 'specialized.com'],
+        domains: ['www.cervelo.com', 'assets.specialized.com', 'www.enve.com', 'enve.com', 'images2.giant-bicycles.com', 'giant-bicycles.com', 'cervelo.com', 'specialized.com'],
     }
 };
 
