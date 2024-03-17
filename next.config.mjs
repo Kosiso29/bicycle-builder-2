875a049d7ae63dff609d22e75e02bfad1ee26d0f
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-03-17 23:12:33 +0100

    17/3/24: add acceptable image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['www.cervelo.com', 'assets.specialized.com', 'enve.com', 'giant-bicycles.com', 'cervelo.com', 'specialized.com'],
+    }
+};
 
 export default nextConfig;
