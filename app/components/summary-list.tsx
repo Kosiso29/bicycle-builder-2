910c961c70ff23c6667aa3dfdc399d2b4cc117fd
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-05-25 18:30:29 +0100

    25/5/24: remove groupsets from summary

--- app/components/summary-list.tsx
+++ app/components/summary-list.tsx
@@ -11,7 +11,7 @@ function SummaryList({ canvasDrawImageProps, frameSetDimensions, small }) {
                 <h1 className={`text-center font-bold ${small ? "text-xl" : "text-4xl"} mb-5`} > Summary</h1>
                 {
                     values.map((item, index) => (
-                        <div key={item.brand + item.model + titles[index]}>
+                        item.brand && <div key={item.brand + item.model + titles[index]}>
                             <div className='flex justify-between py-3'>
                                 <h1 className={`font-bold ${small ? "text-md" : "text-2xl"} basis-[30%]`}>{titles[index]}</h1>
                                 <p className={`basis-[30%] text-primary ${small ? "text-sm" : ""}`}>{item.brand && !(index === 3 && frameSetDimensions.hasStem) && !(index === 4 && frameSetDimensions.hasHandleBar) ? item.brand + " " + item.model : "---"}</p>
@@ -19,7 +19,7 @@ function SummaryList({ canvasDrawImageProps, frameSetDimensions, small }) {
                             </div>
                             <hr className='h-[2px] bg-gray-400' />
                         </div>
-                    )).filter((_, index) => index !== 2)
+                    )).filter((item, index) => item && index !== 2)
                 }
             </div>
         </div >
