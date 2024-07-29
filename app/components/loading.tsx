bd2c6fe0afc42a82a7ba8d34096c9ef6517a3d8f
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-07-29 01:35:02 +0100

    28/7/24: add loading to login button

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function Loading({ small }: { small?: boolean }) {
+export default function Loading({ small, white }: { small?: boolean, white?: boolean }) {
     if (small) {
         return (
             <div className='w-full'>
-                <div className='b border-[4px] border-t-[4px] border-primary border-t-transparent rounded-full w-5 h-5 animate-spin ml-auto mr-auto'></div>
+                <div className={`b border-[4px] border-t-[4px] ${white ? "border-white" : "border-primary"} border-t-transparent rounded-full w-5 h-5 animate-spin ml-auto mr-auto`}></div>
             </div>
         )
     }
 
     return (
         <div className='w-full'>
-            <div className='b border-[6px] border-t-[6px] border-primary border-t-transparent rounded-full w-16 h-16 animate-spin ml-auto mr-auto'></div>
+            <div className={`b border-[6px] border-t-[6px] ${white ? "border-white" : "border-primary"} border-t-transparent rounded-full w-16 h-16 animate-spin ml-auto mr-auto`}></div>
         </div>
     )
 }
--- app/components/login-form.tsx
+++ app/components/login-form.tsx
@@ -1,18 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Button, TextField } from "@mui/material";
 import { ErrorOutline } from "@mui/icons-material";
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
+import Loading from "@/app/components/loading";
 
 export default function Login() {
     const [errorMessage, dispatch] = useFormState(authenticate, undefined);
     const { pending } = useFormStatus();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (errorMessage) {
+            setLoading(false);
+        }
+    }, [errorMessage])
+
+    const handleClick = () => {
+        if (email && password) {
+            setLoading(true);
+        }
+    }
+
     return (
         <form action={dispatch} className="flex flex-col w-[35%] gap-10 bg-blue-100/80 p-16 rounded-3xl">
             <Image
@@ -22,8 +38,8 @@ export default function Login() {
                 className="m-auto rounded-[50%] mb-10"
                 alt="logo"
             />
-            <TextField id="email" name="email" label="Username" variant="standard" type="email" value={email} placeholder="Username" onChange={(e) => setEmail(e.target.value)} />
-            <TextField id="password" name="password" label="Password" variant="standard" type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <TextField id="email" name="email" label="Email" variant="standard" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <TextField id="password" name="password" label="Password" variant="standard" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
             {errorMessage && (
                 <div
                     className="flex items-end space-x-1"
@@ -34,7 +50,16 @@ export default function Login() {
                     <p className="text-sm text-red-500">{errorMessage}</p>
                 </div>
             )}
-            <Button type="submit" aria-disabled={pending} variant="contained" fullWidth>Login</Button>
+            <Button type="submit" onClick={handleClick} aria-disabled={pending} variant="contained" fullWidth>
+                Login
+                {
+                    loading ?
+                        <div className="ml-5">
+                            <Loading small white />
+                        </div>
+                        : null
+                }
+            </Button>
             <Link href="/">
                 <Button variant="outlined" fullWidth>Back to home</Button>
             </Link>
