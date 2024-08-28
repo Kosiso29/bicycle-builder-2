a7121738f551542c4b88a5cb3ebf8f5c95271d6a
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-08-28 23:51:49 +0100

    28/8/24: use query params on admin component tabs

--- app/dashboard/components/page.tsx
+++ app/dashboard/components/page.tsx
@@ -1,18 +1,41 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import SelectionTabs from "@/app/components/selection-tabs";
 import Accessory from "@/app/components/accessory";
 import Inventory from "@/app/components/inventory";
 
 export default function Components() {
-    const [tabSelectionState, setTabSelectionState] = useState("Inventory");
+    const [tabSelectionState, setTabSelectionState] = useState("");
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        if (searchParams.get("tab") === "accessory") {
+            setTabSelectionState("accessory");
+        } else {
+            setTabSelectionState("inventory");
+        }
+    }, [searchParams])
+
+    useEffect(() => {
+        const params = new URLSearchParams();
+        if (tabSelectionState === "accessory") {
+            params.set("tab", "accessory");
+            router.push(`${pathname}?${params.toString()}`);
+        } else {
+            router.push(`${pathname}`);
+        }
+    }, [tabSelectionState, pathname, router])
 
     return (
         <div>
-            <SelectionTabs indexArray={["Inventory", "Accessory"]} value={tabSelectionState} updateSelectionLevel={setTabSelectionState} />
-            { tabSelectionState === "Inventory" && <Inventory /> }
-            { tabSelectionState === "Accessory" && <Accessory /> }
+            <SelectionTabs indexArray={["inventory", "accessory"]} value={tabSelectionState} updateSelectionLevel={setTabSelectionState} />
+            { tabSelectionState === "inventory" && <Inventory /> }
+            { tabSelectionState === "accessory" && <Accessory /> }
         </div>
     );
 }
