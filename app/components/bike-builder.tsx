b618f0281afcc40e0dccd74cde204beec2ca7184
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-09-02 23:19:01 +0100

    2/9/24: add shifter placeholder

--- app/components/bike-builder.tsx
+++ app/components/bike-builder.tsx
@@ -92,7 +92,7 @@ export default function BikeBuilder({
         tire: { image: "/PH-Tan_SES31_FullWheel-modified.png", globalCompositeOperation: 'destination-over' },
         // drivetrain actualWidth used is 622mm instead of 722mm
         groupSet_drivetrain: { image: "/PH-Drivetrain_dura-ace_final.png", x: 185, y: 380, width: 331.733333333, height: 136.6176524785, globalCompositeOperation: 'destination-over' },
-        groupSet_shifter: { image: "/PH-Shifter_dura-ace_final.png", x: 701, y: 121.859649118, width: 80, height: 96.140350882, stemShifterX: 98, stemShifterY: 76, handleBarShifterX: 50, handleBarShifterY: 70, globalCompositeOperation: 'destination-over' },
+        groupSet_shifter: { image: "/PH-dura-ace-shifter.png", x: 701, y: 121.859649118, width: 80, height: 96.140350882, stemShifterX: 98, stemShifterY: 76, handleBarShifterX: 50, handleBarShifterY: 70, globalCompositeOperation: 'destination-over' },
     }
 
     function setImage(doNotRenderCanvasNumbers = false, doNotIncrementCanvasSelectionLevelState = false) {
