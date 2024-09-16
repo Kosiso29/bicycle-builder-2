e07e460c91bd33c3a1c18aad6b22028590777b4b
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-09-16 13:03:08 +0100

    16/9/24: reposition admin button to bottom right

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import Link from "next/link";
 export default function Home() {
 
     return (
-        <main className="h-screen flex items-center justify-center bg-blue-100">
+        <main className="relative h-screen flex items-center justify-center bg-blue-100">
             <div className="flex gap-10">
                 <Link href="/build">
                     <Button variant="contained">New Build</Button>
                 </Link>
-                <Link href="/login">
-                    <Button variant="outlined">Admin</Button>
-                </Link>
                 <Link href="/openai">
                     <Button variant="outlined">Current Bike</Button>
+                </Link>
+            </div>
+            <div className="absolute right-5 bottom-5">
+                <Link href="/login">
+                    <Button variant="outlined">Admin</Button>
                 </Link>
             </div>
         </main>
