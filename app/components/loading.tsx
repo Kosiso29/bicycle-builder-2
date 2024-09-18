e6610b5fc6c024cc3b55a235f7f6200a192b4b96
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-09-18 11:24:59 +0100

    18/9/24: implement the loading icon in CTA button

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -4,14 +4,14 @@ export default function Loading({ small, white }: { small?: boolean, white?: boo
     if (small) {
         return (
             <div className='w-full'>
-                <div className={`b border-[4px] border-t-[4px] ${white ? "border-white" : "border-primary"} border-t-transparent rounded-full w-5 h-5 animate-spin ml-auto mr-auto`}></div>
+                <div className={`b border-[4px] border-t-[4px] ${white ? "border-white" : "border-black"} border-t-transparent rounded-full w-5 h-5 animate-spin ml-auto mr-auto`}></div>
             </div>
         )
     }
 
     return (
         <div className='w-full'>
-            <div className={`b border-[6px] border-t-[6px] ${white ? "border-white" : "border-primary"} border-t-transparent rounded-full w-16 h-16 animate-spin ml-auto mr-auto`}></div>
+            <div className={`b border-[6px] border-t-[6px] ${white ? "border-white" : "border-black"} border-t-transparent rounded-full w-16 h-16 animate-spin ml-auto mr-auto`}></div>
         </div>
     )
 }
--- app/ui/call-to-action.tsx
+++ app/ui/call-to-action.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import Link from "next/link";
+import Loading from "@/app/components/loading";
 
 export default function CallToAction({ href, type, children }: { href: string, type?: any, children: React.ReactNode }) {
+    const [loading, setLoading] = useState(false);
     return (
-        <Link href={href} className='inline-block bg-white text-black rounded-none py-4 px-8' type={type}>
+        <Link href={href} onClick={() => setLoading(true)} className='inline-flex gap-2 bg-white text-black rounded-none py-4 px-8' type={type}>
             {children}
+            {loading && <span className='self-center'><Loading small /></span>}
         </Link>
     )
 }
