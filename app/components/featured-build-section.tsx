3f00516eacaacbf634c219f2d9a7ddb38a4e7f68
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-10-10 23:27:16 +0100

    10/10/24: implement react slick for featured builds

--- app/components/featured-build-section.tsx
+++ app/components/featured-build-section.tsx
@@ -1,6 +1,13 @@
+'use client'
+
 import React from 'react'
 import ProgressBar from '../ui/progress-bar';
 import Image from "next/image";
+import Slider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const builds = [
     {
@@ -36,20 +43,14 @@ export default function FeaturedBuildSection() {
     return (
         <div className='py-20 wrapper-padding'>
             <h1 className='flex justify-center text-2xl font-bold mb-20'>Featured Builds</h1>
-            <div className='flex justify-center'>
-                {
-                    builds.map((build: any) => (
-                        <Card key={build.title} title={build.title} src={build.src} ratings={build.ratings} />
-                    ))
-                }
-            </div>
+            <SimpleSlider />
         </div>
     )
 }
 
 function Card({ title, src, ratings }: { title: string, src: string, ratings: any }) {
     return (
-        <div className='border p-5 w-96'>
+        <div className='flex flex-col justify-between border p-5 w-96 h-[25rem]'>
             <div className='flex justify-between items-center gap-5'>
                 <h2 className='text-2xl font-bold'>{title}</h2>
                 <button className='border-none text-white p-3 bg-[#1A1A1A]'>VIEW&nbsp;BUILD</button>
@@ -71,4 +72,31 @@ function Card({ title, src, ratings }: { title: string, src: string, ratings: an
             </div>
         </div>
     )
+}
+
+export function SimpleSlider() {
+    var settings = {
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: '0px',
+        variableWidth: false,
+        prevArrow: <svg viewBox="0 0 180 310" stroke='#000' strokeWidth={50} role="button" aria-label="previous">
+            <path d="M170 10 L10 161 M10 150 L170 300"></path>
+        </svg>,
+        nextArrow: <svg viewBox="0 0 180 310" stroke='#000' strokeWidth={50} role="button" aria-label="next">
+            <path d="M10 10 L170 161 M170 150 L10 300"></path>
+        </svg>,
+    };
+    return (
+        <Slider {...settings}>
+            {
+                builds.map((build: any) => (
+                    <Card key={build.title} title={build.title} src={build.src} ratings={build.ratings} />
+                ))
+            }
+        </Slider>
+    );
 }
