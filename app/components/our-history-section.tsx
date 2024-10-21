973b779c4cc48c3cbec078a8db0531a74d03b6fe
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-10-22 00:10:10 +0100

    21/10/24: update the our history text

--- app/components/our-history-section.tsx
+++ app/components/our-history-section.tsx
@@ -6,7 +6,9 @@ export default function OurHistory() {
             <div className="pt-5 max-w-[38%]">
                 <div className="mb-10">
                     <h1 className="text-2xl font-bold mb-5">OUR HISTORY</h1>
-                    <p className="text-lg">Founded in [Year], [Your Company Name] began in a small workshop with a simple goal: to craft high-performance, custom-made bikes that balance innovative design with unmatched quality. Every bike we create is the result of meticulous attention to detail, using only the finest materials and cutting-edge technology to ensure every frame, wheel, and component meets the highest standards.</p>
+                    <p className="text-lg">Cyke was born out of a passion for cycling and the belief that every rider deserves a personalized experience. We offer a cutting-edge platform where you can fully customize your dream bike, from frame selection to components. Our interactive configurator lets you visualize your bike in real-time, ensuring a build that’s uniquely yours. Whether you’re chasing performance or aesthetics, Cyke is dedicated to delivering bespoke road bikes tailored to your needs. <br /> <br />
+                        Start your journey with us, and ride the future of cycling.
+                    </p>
                 </div>
                 <div>
                     <h1 className="text-2xl font-bold">OUR VALUES</h1>
