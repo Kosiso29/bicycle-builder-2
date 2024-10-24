202b6fda9c7e66da2d986b7d4e7e01c253e6f26d
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-10-24 17:42:20 +0100

    24/10/24: add header to Checkout and Payment screens

--- app/components/payment.tsx
+++ app/components/payment.tsx
@@ -3,6 +3,7 @@ import { ArrowBackIos, EditOutlined } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import Image from "next/image";
 import PaymentOptions from "@/app/components/payment-options";
+import Header from "@/app/components/header";
 import { useState } from "react";
 
 export default function Payment({ showBilling, setShowBilling, canvasImage, totalPrice, setTotalPrice }: { showBilling: any, setShowBilling: any, canvasImage: string, totalPrice: any, setTotalPrice: any }) {
@@ -30,66 +31,69 @@ export default function Payment({ showBilling, setShowBilling, canvasImage, tota
     }
 
     return (
-        <div className="bg-[#F0EFEF] min-h-screen pt-4 pb-20">
-            <div>
-                <div className='mb-4 pl-10'>
-                    <Button variant="text" onClick={() => setShowBilling(false)}> <ArrowBackIos /> Back</Button>
+        <div className="bg-[#F0EFEF] min-h-screen">
+            <Header />
+            <div className="pt-4 pb-20 wrapper">
+                <div>
+                    <div className='mb-4 -ml-16'>
+                        <Button variant="text" onClick={() => setShowBilling(false)}> <ArrowBackIos /> Back</Button>
+                    </div>
+                    <h1 className='text-2xl font-bold mb-5'>{showPaymentOptions ? "Payment" : "Shipping"}</h1>
                 </div>
-                <h1 className='text-2xl font-bold pl-32 mb-5'>{showPaymentOptions ? "Payment" : "Shipping"}</h1>
-            </div>
-            <div className='flex'>
-                <div className='basis-[50%] max-w-[50%] pl-32'>
-                    {
-                        showPaymentOptions ? <PaymentOptions /> : <ShippingForm setShowPaymentOptions={setShowPaymentOptions} shippingInformation={shippingInformation} setShippingInformation={setShippingInformation} />
-                    }
-                </div>
-                <div className='basis-[50%] flex justify-center'>
-                    <div className='flex flex-col gap-6'>
-                        <div className="w-[20rem] p-10 bg-[#C4C4C480]">
-                            <Image src={canvasImage} style={{ width: "100%", maxWidth: "100%", height: "auto" }} width={0} height={0} alt='' />
-                            <div className="flex flex-col gap-3 py-3">
-                                <h2 className="text-lg font-semibold">Billing Summary</h2>
-                                <p className="flex justify-between">
-                                    <span>Subtotal:</span>
-                                    <span className="font-bold">${Math.round(totalPrice)}</span>
-                                </p>
-                                <p className="flex justify-between">
-                                    <span>Delivery:</span>
-                                    <span className="font-bold">$100</span>
-                                </p>
-                                <p className="flex justify-between">
-                                    <span>Total:</span>
-                                    <span className="font-bold">${Math.round(totalPrice) + 100}</span>
-                                </p>
+                <div className='flex'>
+                    <div className='basis-[50%] max-w-[50%]'>
+                        {
+                            showPaymentOptions ? <PaymentOptions /> : <ShippingForm setShowPaymentOptions={setShowPaymentOptions} shippingInformation={shippingInformation} setShippingInformation={setShippingInformation} />
+                        }
+                    </div>
+                    <div className='basis-[50%] flex justify-end'>
+                        <div className='flex flex-col gap-6'>
+                            <div className="w-[20rem] p-10 bg-[#C4C4C480]">
+                                <Image src={canvasImage} style={{ width: "100%", maxWidth: "100%", height: "auto" }} width={0} height={0} alt='' />
+                                <div className="flex flex-col gap-3 py-3">
+                                    <h2 className="text-lg font-semibold">Billing Summary</h2>
+                                    <p className="flex justify-between">
+                                        <span>Subtotal:</span>
+                                        <span className="font-bold">${Math.round(totalPrice)}</span>
+                                    </p>
+                                    <p className="flex justify-between">
+                                        <span>Delivery:</span>
+                                        <span className="font-bold">$100</span>
+                                    </p>
+                                    <p className="flex justify-between">
+                                        <span>Total:</span>
+                                        <span className="font-bold">${Math.round(totalPrice) + 100}</span>
+                                    </p>
+                                </div>
+                                {
+                                    showPaymentOptions &&
+                                    <div className="flex flex-col gap-3 py-3">
+                                        <h2 className="flex justify-between items-center text-lg font-semibold">Shipping to <EditOutlined className="cursor-pointer" fontSize="small" onClick={() => setShowPaymentOptions(false)} /></h2>
+                                        <p className="flex justify-between">
+                                            {
+                                                shippingInformation.street || shippingInformation.city ?
+                                                    <span>{shippingInformation.street + " " + shippingInformation.city},</span> : null
+                                            }
+                                        </p>
+                                        <p className="flex justify-between">
+                                            {shippingInformation.state && <span>{shippingInformation.state},</span>}
+                                        </p>
+                                        <p className="flex justify-between">
+                                            <span>{shippingInformation.country}</span>
+                                        </p>
+                                        <p className="flex justify-between">
+                                            <span>{shippingInformation.zip}</span>
+                                        </p>
+                                    </div>
+                                }
                             </div>
                             {
-                                showPaymentOptions &&
-                                <div className="flex flex-col gap-3 py-3">
-                                    <h2 className="flex justify-between items-center text-lg font-semibold">Shipping to <EditOutlined className="cursor-pointer" fontSize="small" onClick={() => setShowPaymentOptions(false)} /></h2>
-                                    <p className="flex justify-between">
-                                        {
-                                            shippingInformation.street || shippingInformation.city ?
-                                                <span>{shippingInformation.street + " " + shippingInformation.city},</span> : null
-                                        }
-                                    </p>
-                                    <p className="flex justify-between">
-                                        {shippingInformation.state && <span>{shippingInformation.state},</span>}
-                                    </p>
-                                    <p className="flex justify-between">
-                                        <span>{shippingInformation.country}</span>
-                                    </p>
-                                    <p className="flex justify-between">
-                                        <span>{shippingInformation.zip}</span>
-                                    </p>
+                                !showPaymentOptions &&
+                                <div className="flex justify-center mt-5">
+                                    <Button variant="contained" onClick={handlePaymentClick}>Proceed to Payment</Button>
                                 </div>
                             }
                         </div>
-                        {
-                            !showPaymentOptions &&
-                            <div className="flex justify-center mt-5">
-                                <Button variant="contained" onClick={handlePaymentClick}>Proceed to Payment</Button>
-                            </div>
-                        }
                     </div>
                 </div>
             </div>
--- app/components/summary.tsx
+++ app/components/summary.tsx
@@ -5,6 +5,7 @@ import { Button } from "@mui/material";
 import { ArrowBackIos } from "@mui/icons-material";
 import SummaryList from "@/app/components/summary-list";
 import AddonSummary from './addon-summary';
+import Header from "@/app/components/header";
 
 export default function Summary({ canvasDrawImageProps, canvasImage, showSummary, setShowSummary, frameSetDimensions, addonAccessories, setAddonAccessories, accessoryModels, showBilling, setShowBilling }) {
     // pass in props as parentProps for AddonSummary
@@ -15,27 +16,30 @@ export default function Summary({ canvasDrawImageProps, canvasImage, showSummary
     }
 
     return (
-        <div className='flex bg-[#F0EFEF] min-h-screen pb-16'>
-            <div className='flex flex-col gap-2 basis-[50%] p-5 max-w-[50%] max-h-[100vh] pl-20'>
-                <div className='my-4'>
-                    <Button variant="text" onClick={() => setShowSummary(false)}> <ArrowBackIos /> Back</Button>
-                </div>
-                <h1 className='text-3xl leading-10 font-extrabold'>Reviews and <br /> Checkout</h1>
-                <div className="flex flex-grow items-center slide-in-animation">
-                    <Image src={canvasImage} style={{ width: "100%", height: "auto" }} width={0} height={0} alt='' />
-                </div>
-            </div>
-            <div className='basis-[50%] pt-[10vh] px-32'>
-                <div className='flex flex-col justify-between gap-6 min-h-[80vh]'>
-                    <div className="flex justify-center">
-                        <div className='flex-grow max-w-[30rem]'>
-                            <AddonSummary parentProps={parentProps} />
-                        </div>
+        <div className='bg-[#F0EFEF] min-h-screen'>
+            <Header />
+            <div className='flex pb-16 wrapper'>
+                <div className='flex flex-col gap-2 basis-[50%] p-5 pl-0 max-w-[50%] max-h-[calc(100vh-4rem)]'>
+                    <div className='my-4 -ml-16'>
+                        <Button variant="text" onClick={() => setShowSummary(false)}> <ArrowBackIos /> Back</Button>
                     </div>
-                    <div className="flex justify-center">
-                        <div className="flex justify-end flex-grow mt-5 max-w-[30rem]">
-                            {/* <Button variant="text">Add to Favourites</Button> */}
-                            <Button variant="contained" onClick={() => setShowBilling(true)}>Proceed to Payment</Button>
+                    <h1 className='text-3xl leading-10 font-extrabold'>Reviews and <br /> Checkout</h1>
+                    <div className="flex flex-grow items-center slide-in-animation">
+                        <Image src={canvasImage} style={{ width: "100%", height: "auto" }} width={0} height={0} alt='' />
+                    </div>
+                </div>
+                <div className='basis-[50%] pt-[10vh] px-32'>
+                    <div className='flex flex-col justify-between gap-6 min-h-[calc(80vh-4rem)]'>
+                        <div className="flex justify-center">
+                            <div className='flex-grow max-w-[30rem]'>
+                                <AddonSummary parentProps={parentProps} />
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="flex justify-end flex-grow mt-5 max-w-[30rem]">
+                                {/* <Button variant="text">Add to Favourites</Button> */}
+                                <Button variant="contained" onClick={() => setShowBilling(true)}>Proceed to Payment</Button>
+                            </div>
                         </div>
                     </div>
                 </div>
