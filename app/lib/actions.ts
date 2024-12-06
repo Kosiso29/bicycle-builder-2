0528af31c23e597f2231690d1fd8490d69258ce7
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-12-06 21:47:56 +0100

    6/12/24: implement update for product models

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -276,45 +276,56 @@ export async function updateModel(id: string, formData: any) {
             SET sku = ${sku}, product_type_id = ${product_type_id}, vendor = ${vendor}, buy_price_us = ${buy_price_us}, sell_price_sg = ${price_sg}, sell_price_us = ${price_us}, sell_price_gb = ${price_gb}, sell_price_in = ${price_in}, location = ${location}, lead_time = ${lead_time}
             WHERE id = ${id};
         `
-        // await sql`
-        // UPDATE models
-        // SET category_id = ${category_id}, brand_id = ${brand_id}, name = ${model}, image_url = ${image_url}, actual_width = ${actual_width}, stem_x = ${stem_x}, stem_y = ${stem_y}, saddle_x = ${saddle_x}, saddle_y = ${saddle_y}, front_wheel_x = ${front_wheel_x}, front_wheel_y = ${front_wheel_y}, 
-        // back_wheel_x = ${back_wheel_x}, back_wheel_y = ${back_wheel_y}, has_stem = ${!!has_stem}, has_handle_bar = ${!!has_handle_bar}, price_sg = ${price_sg}, price_gb = ${price_gb}, price_us = ${price_us}, price_in = ${price_in}, key_metrics = ${key_metrics}, aerodynamics = ${aerodynamics}, weight = ${weight}, comfort = ${comfort}, stiffness = ${stiffness}, overall = ${overall}, 
-        // groupset_drivetrain_x = ${groupset_drivetrain_x}, groupset_drivetrain_y = ${groupset_drivetrain_y}, groupset_shifter_x = ${groupset_shifter_x}, groupset_shifter_y = ${groupset_shifter_y}, handle_bar_x = ${handle_bar_x}, handle_bar_y = ${handle_bar_y}, global_composite_operation = ${global_composite_operation}, canvas_layer_level = ${canvas_layer_level},
-        // lengths = ${JSON.parse(lengths)}, sizes = ${JSON.parse(sizes)}, ratios = ${JSON.parse(ratios)}, steerer_size = ${steerer_size}, size_chart_url = ${size_chart_url}, is_primary = ${!!is_primary}, color_name = ${color_name}, color_value = ${color_value}, linked_stem = ${checkForNull(linked_stem)}, linked_handle_bar = ${checkForNull(linked_handle_bar)}, preview_image_url = ${preview_image_url}, canvas_marker_x = ${canvas_marker_x}, canvas_marker_y = ${canvas_marker_y}
-        // WHERE id = ${id};
-        // `
 
-        // // Delete existing presets mapping for the current model so that new ones can be used to overwrite them (this is only required in models update)
-        // await sql`
-        // DELETE FROM models_presets WHERE model_id = ${id};
-        // `
+        const selectedModel: any = await sql`
+            SELECT * FROM models WHERE product_id = ${id} AND is_primary = ${true};
+        `;
 
-        // // Only insert new presets if any were selected, else don't create any presets mapping
-        // if (modelsPresets.length > 0) {
-        //     for (const modelPreset of modelsPresets) {
-        //         const splitModelPreset = modelPreset[1].split("_");
-        //         const model_id = splitModelPreset[0];
-        //         const preset_id = splitModelPreset[1];
-        //         await sql`
-        //         INSERT INTO models_presets (model_id, preset_id) VALUES (${model_id}::uuid, ${preset_id}::uuid);
-        //         `
-        //     }
-        // }
+        const modelId = selectedModel.rows[0]?.id;
 
-        // // Delete existing colors
-        // await sql`
-        //     DELETE FROM colors WHERE model_id = ${id};
-        // `
+        if (!modelId) {
+            throw new Error('Failed to retrieve model ID');
+        }
 
-        // // recreate colors
-        // for (const color_prop of JSON.parse(color_props)) {
-        //     await sql`
-        //         INSERT INTO colors (model_id, name, value, image_url, price_sg, price_gb, price_us, price_in) VALUES (${id}::uuid, ${color_prop.name}, ${color_prop.value}, ${color_prop.image_url}, ${color_prop.price_sg}, ${color_prop.price_gb}, ${color_prop.price_us}, ${color_prop.price_in});
-        //     `
-        // }
+        await sql`
+            UPDATE models
+            SET category_id = ${category_id}, brand_id = ${brand_id}, name = ${model}, image_url = ${image_url}, actual_width = ${actual_width}, stem_x = ${stem_x}, stem_y = ${stem_y}, saddle_x = ${saddle_x}, saddle_y = ${saddle_y}, front_wheel_x = ${front_wheel_x}, front_wheel_y = ${front_wheel_y}, 
+            back_wheel_x = ${back_wheel_x}, back_wheel_y = ${back_wheel_y}, has_stem = ${!!has_stem}, has_handle_bar = ${!!has_handle_bar}, price_sg = ${price_sg}, price_gb = ${price_gb}, price_us = ${price_us}, price_in = ${price_in}, key_metrics = ${key_metrics}, aerodynamics = ${aerodynamics}, weight = ${weight}, comfort = ${comfort}, stiffness = ${stiffness}, overall = ${overall}, 
+            groupset_drivetrain_x = ${groupset_drivetrain_x}, groupset_drivetrain_y = ${groupset_drivetrain_y}, groupset_shifter_x = ${groupset_shifter_x}, groupset_shifter_y = ${groupset_shifter_y}, handle_bar_x = ${handle_bar_x}, handle_bar_y = ${handle_bar_y}, global_composite_operation = ${global_composite_operation}, canvas_layer_level = ${canvas_layer_level},
+            lengths = ${JSON.parse(lengths)}, sizes = ${JSON.parse(sizes)}, ratios = ${JSON.parse(ratios)}, steerer_size = ${steerer_size}, size_chart_url = ${size_chart_url}, is_primary = ${!!is_primary}, color_name = ${color_name}, color_value = ${color_value}, linked_stem = ${checkForNull(linked_stem)}, linked_handle_bar = ${checkForNull(linked_handle_bar)}, preview_image_url = ${preview_image_url}, canvas_marker_x = ${canvas_marker_x}, canvas_marker_y = ${canvas_marker_y}
+            WHERE id = ${modelId};
+        `
 
-        // await autoCalculateRatings();
+        // Delete existing presets mapping for the current model so that new ones can be used to overwrite them (this is only required in models update)
+        await sql`
+            DELETE FROM models_presets WHERE model_id = ${modelId};
+        `
+
+        // Only insert new presets if any were selected, else don't create any presets mapping
+        if (modelsPresets.length > 0) {
+            for (const modelPreset of modelsPresets) {
+                const splitModelPreset = modelPreset[1].split("_");
+                const model_id = splitModelPreset[0];
+                const preset_id = splitModelPreset[1];
+                await sql`
+                INSERT INTO models_presets (model_id, preset_id) VALUES (${model_id}::uuid, ${preset_id}::uuid);
+                `
+            }
+        }
+
+        // Delete existing colors
+        await sql`
+            DELETE FROM colors WHERE model_id = ${modelId};
+        `
+
+        // recreate colors
+        for (const color_prop of JSON.parse(color_props)) {
+            await sql`
+                INSERT INTO colors (model_id, name, value, image_url, price_sg, price_gb, price_us, price_in) VALUES (${modelId}::uuid, ${color_prop.name}, ${color_prop.value}, ${color_prop.image_url}, ${color_prop.price_sg}, ${color_prop.price_gb}, ${color_prop.price_us}, ${color_prop.price_in});
+            `
+        }
+
+        await autoCalculateRatings();
 
         revalidatePath('/dashboard/components');
         revalidatePath(`/dashboard/components/${id}/edit`);
