bb953d2286a2f115d329c89fbf163d4c558433cb
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2024-12-13 13:08:12 +0100

    13/12/24: use different headers for linked component forms

--- app/components/form-container.tsx
+++ app/components/form-container.tsx
@@ -14,14 +14,29 @@ export default function FormContainer() {
 
     return (
         <div>
-            <div className="" style={{ display: !showFormForLinkedComponents ? "block" : "none" }}>
-                <Form key="Form1" linkedStemFormData={linkedStemFormData} linkedHandleBarFormData={linkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} /> :
+            <div style={{ display: !showFormForLinkedComponents ? "block" : "none" }}>
+                <h1 className='text-4xl text-primary'>
+                    Create Component
+                </h1>
+                <div className='bg-white w-full mt-8 rounded-lg md:p-8 py-8 px-2 h-auto'>
+                    <Form key="Form1" linkedStemFormData={linkedStemFormData} linkedHandleBarFormData={linkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} /> :
+                </div>
             </div>
             <div style={{ display: showFormForLinkedComponents === "Stem" ? "block" : "none" }}>
-                <Form key="Form2" setLinkedStemFormData={setLinkedStemFormData} setLinkedHandleBarFormData={setLinkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} />
+                <h1 className='text-4xl text-primary'>
+                    Create Linked Stem
+                </h1>
+                <div className='bg-white w-full mt-8 rounded-lg md:p-8 py-8 px-2 h-auto'>
+                    <Form key="Form2" setLinkedStemFormData={setLinkedStemFormData} setLinkedHandleBarFormData={setLinkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} />
+                </div>
             </div>
             <div style={{ display: showFormForLinkedComponents === "Handle Bar" ? "block" : "none" }}>
-                <Form key="Form3" setLinkedStemFormData={setLinkedStemFormData} setLinkedHandleBarFormData={setLinkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} />
+                <h1 className='text-4xl text-primary'>
+                    Create Linked Handle Bar
+                </h1>
+                <div className='bg-white w-full mt-8 rounded-lg md:p-8 py-8 px-2 h-auto'>
+                    <Form key="Form3" setLinkedStemFormData={setLinkedStemFormData} setLinkedHandleBarFormData={setLinkedHandleBarFormData} showFormForLinkedComponents={showFormForLinkedComponents} setShowFormForLinkedComponents={setShowFormForLinkedComponents} />
+                </div>
             </div>
         </div>
     );
--- app/dashboard/components/create/page.tsx
+++ app/dashboard/components/create/page.tsx
@@ -4,12 +4,7 @@ export default function Page() {
 
     return (
         <div>
-            <h1 className='text-4xl text-primary'>
-                Create Component
-            </h1>
-            <div className='bg-white w-full mt-8 rounded-lg md:p-8 py-8 px-2 h-auto'>
-                <FormContainer />
-            </div>
+            <FormContainer />
         </div>
     );
 }
