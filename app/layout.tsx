25b30aed529d48b54a1cfa1b53b45e66e3e30663
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2025-03-18 09:43:55 +0100

    18/3/25: use cyke.life logo as favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     title: "Cyke",
     description: "At Cyke, we offer road bike enthusiasts a personalized experience to customize their bikes and explore endless possibilities. Upload your current setup, mix and match components, and watch your dream bike come to life in real-time. Whether you want high-end Italian parts or budget-friendly options, Cyke empowers you to build and visualize your perfect bike, providing an ongoing journey of discovery and customization—all at your fingertips.",
     icons: {
-      icon: '/Favicon.png',
+      icon: '/Favicon.ico',
     },
 };
 
