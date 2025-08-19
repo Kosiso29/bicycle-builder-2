98d4737b646fb65276f310e15c32d7fdee466a4a
Kosisochukwu Afoenyi <kafoenyi@gmail.com>
2025-08-19 17:34:09 +0100

    remove the login authentication bypass

--- auth.config.ts
+++ auth.config.ts
@@ -9,8 +9,8 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
-                return true;
-                // return false; // Redirect unauthenticated users to login page
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
             } else if (isLoggedIn && nextUrl.pathname.startsWith('/login')) {
                 return Response.redirect(new URL(`/dashboard?email=${auth.user?.email}`, nextUrl));
             }
